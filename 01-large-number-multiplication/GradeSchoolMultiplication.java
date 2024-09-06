4a68f0bc599205088ac76b49b3952f739be9f780
kjimenez6 <151109667+kjimenez6@users.noreply.github.com>
2024-09-06 12:58:56 -0500

    Update GradeSchoolMultiplication.java

--- 01-large-number-multiplication/GradeSchoolMultiplication.java
+++ 01-large-number-multiplication/GradeSchoolMultiplication.java
@@ -3,6 +3,13 @@ public class GradeSchoolMultiplication {
     private static final int DEFAULT_BASE = 10;
 
     public static int[] multiply(final int[] x, final int[] y, final int base) {
+        int[] product = new int[x.length + y.length]; 
+            for (int i = 0; i < y.length; i++) {
+                for (int a = 0; a < x.length; a++) {
+                    product[ i + a] += y[i] * x[a]; 
+                    product[ i + a + 1] += product [i + a] / base;
+                    product[i + a] %= base;
+                }
     } // method multiply
 
     public static int[] multiply(final int[] x, final int[] y) {
